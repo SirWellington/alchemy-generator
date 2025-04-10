84769d59e3967cb450ec7bb40f41eded927053a3
Wellington Moreno <JWellington.Moreno@Gmail.com>
2025-04-10 09:22:55 -0700

    Refines

--- src/test/java/tech/sirwellington/alchemy/generator/ChecksTest.kt
+++ src/test/java/tech/sirwellington/alchemy/generator/ChecksTest.kt
@@ -19,6 +19,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
+import tech.sirwellington.alchemy.generator.Checks.*
 import tech.sirwellington.alchemy.generator.Throwables.assertThrows
 
 /**
