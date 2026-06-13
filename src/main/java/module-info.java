43eada948b0e08ca11b102a7fec256161d7c3002
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-06-13 12:42:59 -0700

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module tech.sirwellington.alchemy.generator {
+    requires java.base;
+    requires org.slf4j;
+    requires tech.sirwellington.alchemy.annotations;
+
+    exports tech.sirwellington.alchemy.generator;
+}
